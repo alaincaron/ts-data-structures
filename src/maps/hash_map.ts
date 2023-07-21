830391a40f0511b9e7a6af137364700c11d6f545
Alain Caron <caron.alain@gmail.com>
2023-07-21 12:30:46 -0400

    Implemeting rehash

--- src/maps/hash_map.ts
+++ src/maps/hash_map.ts
@@ -1,6 +1,6 @@
 import { AbstractMap } from './abstract_map';
 import { MapOptions, MapComparators } from './types';
-import { nextPrime, cyrb53, Predicate } from '../utils';
+import { nextPrime, cyrb53, Predicate, LARGEST_PRIME } from '../utils';
 
 interface HashEntry<K, V> {
   key: K;
@@ -11,14 +11,18 @@ interface HashEntry<K, V> {
 
 export interface HashMapOptions<K, V> extends MapOptions<K, V> {
   hash?: (k: K) => number;
+  loadFactor?: number;
 }
 
 const MIN_INITIAL_CAPACITY = nextPrime(5);
+const DEFAULT_LOAD_FACTOR = 0.75;
+
 export class HashMap<K, V> extends AbstractMap<K, V> {
   private _size: number;
   private _capacity: number;
   private slots: Array<HashEntry<K, V> | undefined>;
   private readonly hash: (k: K) => number;
+  private readonly loadFactor: number;
 
   private static toOptions<K, V>(initializer: any): MapComparators<K, V> | undefined {
     if (initializer?.equalK || initializer?.equalV) return initializer;
@@ -30,6 +34,7 @@ export class HashMap<K, V> extends AbstractMap<K, V> {
 
     this._size = 0;
     this.hash = cyrb53 as (k: K) => number;
+    this.loadFactor = DEFAULT_LOAD_FACTOR;
 
     if (initializer == null) {
       this.slots = new Array(MIN_INITIAL_CAPACITY);
@@ -38,12 +43,17 @@ export class HashMap<K, V> extends AbstractMap<K, V> {
       this.slots = new Array(nextPrime(Math.max(initializer, MIN_INITIAL_CAPACITY)));
       this._capacity = Infinity;
     } else if (initializer instanceof HashMap) {
+      this.loadFactor = initializer.loadFactor;
       this._size = initializer._size;
       this._capacity = initializer._capacity;
       this.hash = initializer.hash;
       this.slots = new Array(initializer.slots.length);
       this.putAll(initializer);
     } else {
+      if (initializer.loadFactor != null) {
+        if (initializer.loadFactor <= 0.0) throw new Error(`Invalid load factor: ${initializer.loadFactor}`);
+        this.loadFactor = initializer.loadFactor;
+      }
       const capacity = initializer.capacity ?? Infinity;
       const initialElements = initializer.initial;
       if (!initialElements) {
@@ -69,15 +79,15 @@ export class HashMap<K, V> extends AbstractMap<K, V> {
     return this._capacity;
   }
 
-  private getSlot(h: number): number {
-    h = h % this.slots.length;
-    if (h < 0) h += this.slots.length;
+  private getSlot(h: number, slots: Array<HashEntry<K, V> | undefined>): number {
+    h = h % slots.length;
+    if (h < 0) h += slots.length;
     return h;
   }
 
   private getEntry(key: K): HashEntry<K, V> | undefined {
     const h = this.hash(key);
-    const slot = this.getSlot(h);
+    const slot = this.getSlot(h, this.slots);
     let e = this.slots[slot];
     while (e && !(e.hash === h && this.equalK(e.key, key))) e = e.next;
     return e;
@@ -89,7 +99,7 @@ export class HashMap<K, V> extends AbstractMap<K, V> {
 
   put(key: K, value: V): V | undefined {
     const h = this.hash(key);
-    const slot = this.getSlot(h);
+    const slot = this.getSlot(h, this.slots);
     let prev: HashEntry<K, V> | undefined = undefined;
     let e = this.slots[slot];
     while (e && !(e.hash === h && this.equalK(e.key, key))) {
@@ -104,6 +114,7 @@ export class HashMap<K, V> extends AbstractMap<K, V> {
       } else {
         this.slots[slot] = e;
       }
+      if (this.slots.length * this.loadFactor < this._size) this.rehash();
       return undefined;
     } else {
       const old = e.value;
@@ -112,13 +123,34 @@ export class HashMap<K, V> extends AbstractMap<K, V> {
     }
   }
 
+  private rehash() {
+    let newLength = this.slots.length * 1.5;
+    if (newLength < 0 || newLength >= LARGEST_PRIME) {
+      newLength = LARGEST_PRIME;
+    } else {
+      newLength = nextPrime(newLength);
+    }
+    const newSlots = new Array(newLength);
+    for (let i = 0; i < this.slots.length; ++i) {
+      let e = this.slots[i];
+      while (e) {
+        const next = e.next;
+        const slot = this.getSlot(e.hash, newSlots);
+        e.next = newSlots[slot];
+        newSlots[slot] = e;
+        e = next;
+      }
+    }
+    this.slots = newSlots;
+  }
+
   containsKey(key: K): boolean {
     return !!this.getEntry(key);
   }
 
   remove(key: K): V | undefined {
     const h = this.hash(key);
-    const slot = this.getSlot(h);
+    const slot = this.getSlot(h, this.slots);
     let prev: HashEntry<K, V> | undefined = undefined;
     let e = this.slots[slot];
     while (e && !(e.hash === h && this.equalK(e.key, key))) {
