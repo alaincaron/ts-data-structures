8db4bc8c00cb4f43d4e4c553db80215a92088cba
Alain Caron <caron.alain@gmail.com>
2024-02-01 23:44:40 -0500

    Removing unused declarations

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,4 @@
-export type BinaryPredicate<E1, E2 = E1> = (item1: E1, item2: E2) => boolean;
 export type HashFunction<E> = (e: E) => number;
-export type EqualFunction<E> = BinaryPredicate<E>;
 
 export interface RandomAccess<E> {
   getAt(idx: number): E;
