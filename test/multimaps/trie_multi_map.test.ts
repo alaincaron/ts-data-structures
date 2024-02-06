5f63ee8b02271b174b9e68e9ecfb0418d9a45516
Alain Caron <caron.alain@gmail.com>
2024-02-06 01:05:20 -0500

    Adding unit tests for TrieSet and TrieMutiMap

---
+++ test/multimaps/trie_multi_map.test.ts
@@ -0,0 +1,343 @@
+import { expect } from 'chai';
+import { Comparators } from 'ts-fluent-iterators';
+import { list } from './helper';
+import { Collection, MapEntry, OverflowException, TrieMultiMap } from '../../src';
+
+describe('TrieMultiMap', () => {
+  describe('constructor', () => {
+    it('should have infinite capacity as per default ctor', () => {
+      const map = new TrieMultiMap();
+      expect(map.capacity()).equal(Infinity);
+      expect(map.size()).equal(0);
+      expect(map.remaining()).equal(Infinity);
+      expect(map.isEmpty()).to.be.true;
+      expect(map.isFull()).to.be.false;
+    });
+
+    it('should have specified capacity as unique argument', () => {
+      const map = new TrieMultiMap(2);
+      expect(map.capacity()).equal(2);
+      expect(map.size()).equal(0);
+      expect(map.remaining()).equal(2);
+      expect(map.isEmpty()).to.be.true;
+      expect(map.isFull()).to.be.false;
+    });
+
+    it('should use the specified capacity as per options', () => {
+      const map = new TrieMultiMap({ capacity: 2 });
+      expect(map.capacity()).equal(2);
+      expect(map.isEmpty()).to.be.true;
+    });
+
+    it('should initialize with the provided Map', () => {
+      const map = TrieMultiMap.create({ initial: new Map().set('a', 1).set('b', 2) });
+      expect(map.size()).equal(2);
+      expect(map.get('a')?.equals(list(1))).to.be.true;
+      expect(map.get('b')?.equals(list(2))).to.be.true;
+    });
+
+    it('should initialize with the provided IMap', () => {
+      const map1 = new TrieMultiMap();
+      map1.put('a', 1);
+      map1.put('b', 2);
+      const map = TrieMultiMap.create({ initial: map1 });
+      expect(map.size()).equal(2);
+      expect(map.get('a')?.equals(list(1))).to.be.true;
+      expect(map.get('b')?.equals(list(2))).to.be.true;
+    });
+
+    it('should initialize with the provided Iterable', () => {
+      const map = TrieMultiMap.create({
+        initial: [
+          ['a', 1],
+          ['b', 2],
+          ['a', 3],
+          ['b', 4],
+        ] as Array<[string, number]>,
+      });
+      expect(map.size()).equal(4);
+      expect(map.get('a')?.equals(list(1, 3))).to.be.true;
+      expect(map.get('b')?.equals(list(2, 4))).to.be.true;
+    });
+  });
+
+  describe('put/get', () => {
+    it('should return undefined if key is newly added', () => {
+      const map = new TrieMultiMap();
+      expect(map.put('foo', 4)).to.be.true;
+      expect(map.size()).equal(1);
+      expect(map.get('foo')?.equals(list(4))).to.be.true;
+    });
+    it('should return the old value if key already present', () => {
+      const map = new TrieMultiMap();
+      expect(map.put('foo', 4)).to.be.true;
+      expect(map.put('foo', 2)).to.be.true;
+      expect(map.size()).equal(2);
+      expect(map.get('foo')?.equals(list(4, 2))).to.be.true;
+    });
+
+    it('should throw if adding a new element and map is full', () => {
+      const map = new TrieMultiMap(1);
+      expect(map.put('foo', 1)).to.be.true;
+      expect(() => map.put('bar', 1)).to.throw(OverflowException);
+      expect(map.isFull()).to.be.true;
+      expect(map.size()).equal(1);
+    });
+  });
+
+  describe('offer', () => {
+    it('should return true if key is accpeted and added', () => {
+      const map = new TrieMultiMap();
+      expect(map.offer('foo', 4)).to.be.true;
+      expect(map.size()).equal(1);
+      expect(map.get('foo')?.equals(list(4))).to.be.true;
+    });
+    it('should return true if key already present', () => {
+      const map = new TrieMultiMap();
+      expect(map.put('foo', 4)).to.be.true;
+      expect(map.offer('foo', 2)).to.be.true;
+      expect(map.size()).equal(2);
+      expect(map.get('foo')?.equals(list(4, 2))).to.be.true;
+    });
+
+    it('should return false if map is full', () => {
+      const map = new TrieMultiMap(1);
+      expect(map.put('foo', 1)).to.be.true;
+      expect(map.offer('foo', 1)).to.be.false;
+      expect(map.isFull()).to.be.true;
+      expect(map.size()).equal(1);
+    });
+  });
+
+  describe('clone', () => {
+    it('should create a deep equal copy', () => {
+      const a = new TrieMultiMap();
+      a.put('foo', 1);
+      const b = a.clone();
+      expect(b).to.deep.equal(a);
+      b.put('bar', 1);
+      expect(b.size()).equal(2);
+      expect(a.size()).equal(1);
+    });
+  });
+
+  describe('clear', () => {
+    it('should clear the content', () => {
+      const map = new TrieMultiMap({ capacity: 3 });
+      map.put('a', 1);
+      map.put('b', 2);
+      expect(map.size()).to.equal(2);
+      expect(map.remaining()).to.equal(1);
+      map.clear();
+      expect(map.size()).to.equal(0);
+      expect(map.remaining()).to.equal(3);
+    });
+  });
+
+  describe('containsKey', () => {
+    it('should return false on empty map', () => {
+      const map = new TrieMultiMap();
+      expect(map.containsKey('foo')).to.be.false;
+    });
+    it('should return false if absent', () => {
+      const map = new TrieMultiMap();
+      map.put('foo', 1);
+      expect(map.containsKey('bar')).to.be.false;
+    });
+    it('should return true if present', () => {
+      const map = new TrieMultiMap();
+      map.put('foo', 1);
+      expect(map.containsKey('foo')).to.be.true;
+    });
+  });
+
+  describe('containsValue', () => {
+    it('should return false on empty map', () => {
+      const map = new TrieMultiMap();
+      expect(map.containsValue('foo')).to.be.false;
+    });
+    it('should return false if absent', () => {
+      const map = new TrieMultiMap();
+      map.put('foo', 1);
+      expect(map.containsValue('bar')).to.be.false;
+    });
+    it('should return true if present', () => {
+      const map = new TrieMultiMap();
+      map.put('foo', 1);
+      expect(map.containsValue(1)).to.be.true;
+    });
+  });
+
+  describe('remove', () => {
+    it('should return undefined on empty map', () => {
+      const map = new TrieMultiMap();
+      expect(map.removeKey('foo')).to.be.undefined;
+      expect(map.isEmpty()).to.be.true;
+      expect(map.size()).equal(0);
+    });
+    it('should return false if item is missing', () => {
+      const map = new TrieMultiMap();
+      expect(map.put('foo', 1)).to.be.true;
+      expect(map.removeKey('bar')).to.be.undefined;
+      expect(map.isEmpty()).to.be.false;
+      expect(map.size()).equal(1);
+      expect(map.removeKey('foo')?.equals(list(1))).to.be.true;
+    });
+  });
+
+  describe('removeEntry', () => {
+    it('should return undefined on empty map', () => {
+      const map = new TrieMultiMap();
+      expect(map.removeEntry('foo', 1)).to.be.false;
+      expect(map.isEmpty()).to.be.true;
+      expect(map.size()).equal(0);
+    });
+    it('should return false if item is missing', () => {
+      const map = new TrieMultiMap();
+      expect(map.put('foo', 1)).to.be.true;
+      expect(map.removeEntry('foo', 2)).to.be.false;
+      expect(map.isEmpty()).to.be.false;
+      expect(map.size()).equal(1);
+      expect(map.removeEntry('foo', 1)).to.be.true;
+      expect(map.isEmpty()).to.be.true;
+    });
+  });
+
+  describe('filterKeys', () => {
+    it('should remove keys not matching predicate', () => {
+      const map = new TrieMultiMap<number>();
+      map.put('foo', 1);
+      map.put('bar', 2);
+      map.put('foobar', 3);
+      expect(map.filterKeys(k => k.startsWith('b'))).equal(2);
+      expect(map.size()).equal(1);
+      expect(map.containsKey('foo')).to.be.false;
+      expect(map.containsKey('foobar')).to.be.false;
+      expect(map.containsKey('bar')).to.be.true;
+    });
+  });
+
+  describe('filterValues', () => {
+    it('should remove values not matching predicate', () => {
+      const map = new TrieMultiMap<number>();
+      map.put('foo', 1);
+      map.put('bar', 2);
+      map.put('foobar', 3);
+      expect(map.filterValues(v => v % 2 === 0)).equal(2);
+      expect(map.size()).equal(1);
+      expect(map.containsKey('foo')).to.be.false;
+      expect(map.containsKey('foobar')).to.be.false;
+      expect(map.containsKey('bar')).to.be.true;
+    });
+  });
+
+  describe('containsEntry', () => {
+    it('should return whether an entry is present or not', () => {
+      const map = new TrieMultiMap<number>();
+      map.put('foo', 1);
+      expect(map.containsEntry('foo', 1)).to.be.true;
+      expect(map.containsEntry('foo', 2)).to.be.false;
+      expect(map.containsEntry('foobar', 1)).to.be.false;
+    });
+  });
+
+  describe('iterators', () => {
+    const map = new TrieMultiMap<number>();
+    map.put('foo', 1);
+    map.put('bar', 2);
+    map.put('foobar', 3);
+    map.put('foo', 4);
+    map.put('foobar', 5);
+    const sortedKeys = ['bar', 'foo', 'foobar'];
+    it('keys should return all the keys in sorted order', () => {
+      expect(Array.from(map.keys())).to.deep.equal(sortedKeys);
+    });
+    it('keyIterator should return all the keys in sorted order', () => {
+      expect(map.keyIterator().collect()).to.deep.equal(sortedKeys);
+    });
+    it('entryIterator should return all entries sorted according to their key', () => {
+      expect(map.entryIterator().collect()).to.deep.equal([
+        ['bar', 2],
+        ['foo', 1],
+        ['foo', 4],
+        ['foobar', 3],
+        ['foobar', 5],
+      ]);
+    });
+    it('valueIterator should return all values ordered according to the their key order', () => {
+      expect(map.valueIterator().collect()).to.deep.equal([2, 1, 4, 3, 5]);
+    });
+    it('partitionIterator should return partitions ordered according to their key order', () => {
+      const result = map.partitionIterator().collect();
+      expect(result.length === 3);
+      expect(result[0][0]).equal('bar');
+      expect(result[0][1]?.equals(list(2))).to.be.true;
+      expect(result[1][0]).equal('foo');
+      expect(result[1][1]?.equals(list(1, 4))).to.be.true;
+      expect(result[2][0]).equal('foobar');
+      expect(result[2][1]?.equals(list(3, 5))).to.be.true;
+    });
+  });
+
+  describe('navigation methods', () => {
+    it('should return undefined on empty map', () => {
+      const map = new TrieMultiMap<number>();
+      expect(map.firstEntry()).to.be.undefined;
+      expect(map.firstKey()).to.be.undefined;
+      expect(map.lastEntry()).to.be.undefined;
+      expect(map.lastKey()).to.be.undefined;
+      expect(map.reverseEntryIterator().collect()).to.deep.equal([]);
+      expect(map.reverseKeyIterator().collect()).to.deep.equal([]);
+      expect(map.reverseValueIterator().collect()).to.deep.equal([]);
+    });
+
+    it('should return right navigation values', () => {
+      const map = new TrieMultiMap<number>();
+      map.put('bar', 1);
+      map.put('foo', 2);
+      map.put('bar', 3);
+      map.put('foo', 4);
+
+      const barValue = list(1, 3);
+      const fooValue = list(2, 4);
+
+      expect(map.firstKey()).equal('bar');
+      let e = map.firstEntry()!;
+      expect(e.key).equal('bar');
+      expect(e.value.equals(barValue)).to.be.true;
+
+      expect(map.lastKey()).equal('foo');
+      e = map.lastEntry()!;
+      expect(e?.key).equal('foo');
+      expect(e?.value?.equals(fooValue)).to.be.true;
+
+      expect(map.reverseEntryIterator().collect()).satisfies((arr: MapEntry<string, Collection<number>>[]) => {
+        expect(arr.length).equal(2);
+        expect(arr[0].key).equal('foo');
+        expect(arr[0].value.equals(fooValue)).to.be.true;
+        expect(arr[1].key).equal('bar');
+        expect(arr[1].value.equals(barValue)).to.be.true;
+        return true;
+      });
+
+      expect(map.reverseKeyIterator().collect()).to.deep.equal(['foo', 'bar']);
+
+      expect(map.reverseValueIterator().collect()).to.deep.equal(fooValue.iterator().append(barValue).collect());
+    });
+
+    it('should respect the passed comparator', () => {
+      const map = new TrieMultiMap<number>({ comparator: Comparators.reverseComparator });
+      map.put('bar', 1);
+      map.put('foo', 2);
+      map.put('bar', 3);
+      map.put('foo', 4);
+      expect(map.firstKey()).equal('foo');
+      let e = map.firstEntry()!;
+      expect(e.key).equal('foo');
+      expect(e.value!.equals(list(2, 4))).to.be.true;
+      e = map.lastEntry()!;
+      expect(e.key).equal('bar');
+      expect(e.value!.equals(list(1, 3))).to.be.true;
+    });
+  });
+});
---
+++ test/sets/trie_set.test.ts
@@ -0,0 +1,252 @@
+import { expect } from 'chai';
+import { OverflowException, TrieSet } from '../../src';
+
+describe('TrieSet', () => {
+  describe('constructor', () => {
+    it('should have infinite capacity as per default ctor', () => {
+      const set = new TrieSet();
+      expect(set.capacity()).equal(Infinity);
+      expect(set.size()).equal(0);
+      expect(set.remaining()).equal(Infinity);
+      expect(set.isEmpty()).to.be.true;
+      expect(set.isFull()).to.be.false;
+    });
+
+    it('should have specified capacity as unique argument', () => {
+      const set = new TrieSet(2);
+      expect(set.capacity()).equal(2);
+      expect(set.size()).equal(0);
+      expect(set.remaining()).equal(2);
+      expect(set.isEmpty()).to.be.true;
+      expect(set.isFull()).to.be.false;
+    });
+
+    it('should use the specified capacity as per options', () => {
+      const set = TrieSet.create({ capacity: 2 });
+      expect(set.capacity()).equal(2);
+      expect(set.isEmpty()).to.be.true;
+    });
+
+    it('should have the same elements as the array argument', () => {
+      const arr = ['foo', 'bar', 'foobar'];
+      const set = TrieSet.create({ capacity: arr.length, initial: arr });
+      expect(set.capacity()).equal(arr.length);
+      expect(set.size()).equal(arr.length);
+      expect(set.remaining()).equal(0);
+      expect(set.isEmpty()).to.be.false;
+      expect(set.isFull()).to.be.true;
+      expect(set.toArray()).to.deep.equal(arr.sort());
+    });
+
+    it('should be identical to the TrieSet argument', () => {
+      const arr = ['foo', 'bar', 'foobar'];
+      const set1 = TrieSet.create({ capacity: arr.length, initial: arr });
+      expect(set1.capacity()).equal(arr.length);
+      const set2 = TrieSet.create({ initial: set1 });
+      expect(set2.capacity()).equal(arr.length);
+      expect(set1.equals(set2)).to.be.true;
+      expect(set2.toArray()).to.deep.equal(set1.toArray());
+    });
+
+    it('should be identical to the Collection argument', () => {
+      const arr = ['foo', 'foobar', 'bar'];
+      const set1 = TrieSet.create({ initial: arr });
+      const set2 = TrieSet.create({ initial: set1 });
+      expect(set2.capacity()).equal(Infinity);
+      expect(set1.equals(set2)).to.be.true;
+      expect(set2.toArray()).to.deep.equal(arr.sort());
+    });
+  });
+
+  describe('add', () => {
+    it('should return false if element already present', () => {
+      const set = new TrieSet();
+      expect(set.add('foo')).to.be.true;
+      expect(set.add('bar')).to.be.true;
+      expect(set.add('foo')).to.be.false;
+      expect(set.add('bar')).to.be.false;
+      expect(set.size()).equal(2);
+      expect(set.toArray()).to.deep.equal(['bar', 'foo']);
+    });
+    it('should throw if full and element not present', () => {
+      const set = new TrieSet(1);
+      expect(set.add('foo')).to.be.true;
+      expect(set.isFull()).to.be.true;
+      expect(set.add('foo')).to.be.false;
+      expect(() => set.add('bar')).to.throw(OverflowException);
+      expect(set.size()).equal(1);
+    });
+  });
+
+  describe('offer', () => {
+    it('should refuse if full and element not present', () => {
+      const set = new TrieSet(1);
+      expect(set.offer('foo')).to.be.true;
+      expect(set.offer('bar')).to.be.false;
+      expect(set.isFull()).to.be.true;
+      expect(set.offer('foo')).to.be.true;
+      expect(set.size()).equal(1);
+      expect(set.isFull()).to.be.true;
+    });
+  });
+
+  describe('clone', () => {
+    it('should create a deep equal copy', () => {
+      const a = new TrieSet();
+      const b = a.clone();
+      expect(b).to.deep.equal(a);
+      b.add('foo');
+      expect(b.size()).equal(1);
+      expect(a.size()).equal(0);
+    });
+  });
+
+  describe('clear', () => {
+    it('should clear the content', () => {
+      const set = TrieSet.create({ capacity: 3, initial: ['foo', 'bar'] });
+      expect(set.size()).to.equal(2);
+      expect(set.remaining()).to.equal(1);
+      set.clear();
+      expect(set.size()).to.equal(0);
+      expect(set.remaining()).to.equal(3);
+      expect(set.toArray()).to.deep.equal([]);
+    });
+  });
+
+  describe('contains', () => {
+    it('should return false on empty set', () => {
+      const set = new TrieSet();
+      expect(set.contains('foo')).to.be.false;
+    });
+    it('should return false if absent', () => {
+      const set = TrieSet.create({ initial: ['foo', 'bar'] });
+      expect(set.contains('foobar')).to.be.false;
+    });
+    it('should return true if present', () => {
+      const set = TrieSet.create({ initial: ['foo', 'bar'] });
+      expect(set.contains('foo')).to.be.true;
+    });
+  });
+
+  describe('find', () => {
+    it('should return undefined on empty set', () => {
+      const set = new TrieSet();
+      expect(set.find(x => x === 'foo')).to.be.undefined;
+    });
+    it('should return undefined if no match', () => {
+      const set = TrieSet.create({ initial: ['foo', 'bar'] });
+      expect(set.find(x => x === 'foobar')).to.be.undefined;
+    });
+    it('should return the first item matching the predicate', () => {
+      const set = TrieSet.create({ initial: ['foo', 'bar', 'foobar'] });
+      expect(set.find(x => x.length >= 5)).equal('foobar');
+    });
+  });
+
+  describe('removeItem', () => {
+    it('should return false on empty set', () => {
+      const set = new TrieSet();
+      expect(set.removeItem('foobar')).to.be.false;
+      expect(set.isEmpty()).to.be.true;
+      expect(set.size()).equal(0);
+    });
+    it('should return false if item is missing', () => {
+      const arr = ['foo', 'bar', 'foobar'];
+      const set = TrieSet.create({ initial: arr });
+      expect(set.removeItem('baz')).to.be.false;
+      expect(set.isEmpty()).to.be.false;
+      expect(set.size()).equal(3);
+    });
+    it('should remove occurence and return true if item is present', () => {
+      const arr = ['foo', 'bar', 'baz', 'foo', 'foobar', 'baz', 'bar'];
+      const set = TrieSet.create({ initial: arr });
+      expect(set.toArray()).to.deep.equal(['bar', 'baz', 'foo', 'foobar']);
+      expect(set.removeItem('baz')).to.be.true;
+      expect(set.size()).equal(3);
+      expect(set.toArray()).to.deep.equal(['bar', 'foo', 'foobar']);
+    });
+  });
+
+  describe('filter', () => {
+    it('should return 0 on empty set', () => {
+      const set = new TrieSet();
+      expect(set.filter(s => s === 'foobar')).equal(0);
+      expect(set.isEmpty()).to.be.true;
+      expect(set.size()).equal(0);
+    });
+
+    it('should return 0 if all items match the predicate', () => {
+      const arr = ['foo', 'bar', 'baz'];
+      const set = TrieSet.create({ initial: arr });
+      expect(set.filter(s => s.length === 3)).equal(0);
+      expect(set.isEmpty()).to.be.false;
+      expect(set.size()).equal(3);
+    });
+    it('should remove all items not matching the filter', () => {
+      const arr = ['foo', 'bar', 'foobar', 'baz'];
+      const set = TrieSet.create({ initial: arr });
+      expect(set.filter(s => s.length <= 3)).equal(1);
+      expect(set.isEmpty()).to.be.false;
+      expect(set.size()).equal(3);
+      expect(set.toArray()).deep.equal(['bar', 'baz', 'foo']);
+    });
+  });
+
+  describe('offerFully', () => {
+    it('should refuse all the items if not enough capacity remaining', () => {
+      const set = new TrieSet(2);
+      const data = ['foo', 'bar', 'foobar'];
+      expect(set.offerFully(data)).equal(0);
+      expect(set.isEmpty()).to.be.true;
+      expect(set.offerFully(TrieSet.create({ initial: data }))).equal(0);
+      expect(set.isEmpty()).to.be.true;
+    });
+    it('should accept all items if enough capacity remaining', () => {
+      const set = new TrieSet(6);
+      const data = ['foo', 'bar', 'foobar', 'bar', 'foo', 'foobar'];
+      expect(set.offerFully(data)).equal(3);
+      expect(set.size()).equal(3);
+      expect(set.offerFully(TrieSet.create({ initial: ['foo', '1', '2', '3'] }))).equal(3);
+      expect(set.size()).equal(6);
+    });
+    it('should accept all the items if there is enough capacity remaining for distinct elements', () => {
+      const set = new TrieSet(2);
+      const data = ['1', '2', '1', '2'];
+      expect(set.offerFully(data)).equal(2);
+      expect(set.isEmpty()).to.be.false;
+      expect(set.size()).equal(2);
+    });
+  });
+
+  describe('offerPartially', () => {
+    it('should accept elements up to the remaining capacity', () => {
+      const set = new TrieSet(2);
+      const data = ['2', '1', '2', '3'];
+      expect(set.offerPartially(data)).equal(2);
+      expect(set.toArray()).to.deep.equal(['1', '2']);
+    });
+    it('should accept all items if enough capacity remaining', () => {
+      const set = new TrieSet(6);
+      const data = ['1', '1', '2', '2', '3', '4', '5'];
+      expect(set.offerPartially(data)).equal(5);
+      expect(set.size()).equal(5);
+      expect(set.offerPartially(TrieSet.create({ initial: data }))).equal(0);
+      expect(set.size()).equal(5);
+    });
+    it('should accept all the items if there is enough capacity remaining for distinct elements', () => {
+      const set = new TrieSet(2);
+      const data = ['2', '1', '1', '1', '2', '1', '2', '3', '4'];
+      expect(set.offerPartially(data)).equal(2);
+      expect(set.isEmpty()).to.be.false;
+      expect(set.size()).equal(2);
+      expect(set.toArray()).to.deep.equal(['1', '2']);
+    });
+  });
+
+  describe('toJson', () => {
+    it('should return the JSON string', () => {
+      const set = TrieSet.create({ initial: ['foo', 'foobar', 'foo', 'baz', 'bar'] });
+      expect(set.toJson()).equals('["bar","baz","foo","foobar"]');
+    });
+  });
+});
